d22dab3c5e077590336b5bf0a839584f6418ac96
kcchaha <kcchaha@gmail.com>
2019-06-04 16:17:11 +0000

    create components, settle all the imports and exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import MessageList from '../src/MessageList.jsx';
+import ChatBar from '../src/ChatBar.jsx';import Message from './Message.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      <MessageList/>
+      <ChatBar />
+      </div>
+    )
   }
 }
 export default App;
